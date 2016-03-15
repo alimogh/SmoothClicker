49eee9bacc462142840691cb41b66d20b0cfc9b9
pylapp <dev@pylapersonne.info>
2016-03-15 15:05:28 +0100

    feat: Add credits / licenses / thirs-parties libs view
    Signed-off-by: Pierre-Yves Lapersonne <dev@pylapersonne.info>
    
    Signed-off-by: Pierre-Yves Lapersonne <dev@pylapersonne.info>

--- app/src/main/java/com/pylapp/smoothclicker/view/CreditsActivity.java
+++ app/src/main/java/com/pylapp/smoothclicker/view/CreditsActivity.java
@@ -1,4 +1,4 @@
-<!--
+/*
     Copyright 2016 Pierre-Yves Lapersonne (aka. "pylapp",  pylapp(dot)pylapp(at)gmail(dot)com)
 
     Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,11 +12,29 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
---><!-- ✿✿✿✿ ʕ •ᴥ•ʔ/ ︻デ═一 -->
+ */
+// ✿✿✿✿ ʕ •ᴥ•ʔ/ ︻デ═一
 
-<resources>
-    <!-- Default screen margins, per the Android Design guidelines. -->
-    <dimen name="activity_horizontal_margin">16dp</dimen>
-    <dimen name="activity_vertical_margin">16dp</dimen>
-    <dimen name="fab_margin">16dp</dimen>
-</resources>
+package com.pylapp.smoothclicker.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.pylapp.smoothclicker.R;
+
+
+/**
+ * The activity which displays the credits / third-parties licences.
+ *
+ * @author pylapp
+ * @version 1.0.0
+ * @since 15/03/2016
+ */
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+
+}
